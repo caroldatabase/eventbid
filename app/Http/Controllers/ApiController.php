789f6a0c4019b383e74f337b4f3e64d16d512ef9
krsdata <krsdata.net@gmail.com>
2017-06-17 04:43:55 +0530

    custom cat

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -21,6 +21,7 @@ use App\PostTask;
 use App\SubCategory;
 use App\Category;
 use App\CustomCategory;
+use App\CategoryQuestion;
 
 
 
@@ -183,8 +184,6 @@ class ApiController extends Controller
                 break;
             }
         }
-
-      //  dd($cname);
        
         $validator = Validator::make($request->all(), [
            'categoryName'     => "required" ,  
@@ -347,10 +346,10 @@ class ApiController extends Controller
                     }
                             
             return Response::json(array(
-                'status' => 0,
-                'code'   => 500,
-                'message' => $error_msg[0],
-                'data'  =>  $request->all()
+                'status'    => 0,
+                'code'      => 500,
+                'message'   => $error_msg[0],
+                'data'      => $request->all()
                 )
             );
         }  
@@ -363,6 +362,23 @@ class ApiController extends Controller
         $postTask->category         =  $request->get('category');
         $postTask->inspiration_photo=  $request->get('inspirationPhoto');
         $postTask->save();
+
+        $postTask->post_task_id = $postTask->id;
+
+        //post task question
+        $q = [];
+        foreach ($request->get('category_question') as $key => $result) {
+           $task_post_id = $postTask->id;
+           $category_question = new CategoryQuestion;
+           $category_question->category_id              =   $request->get('category');
+           $category_question->post_task_id             =   $task_post_id;
+           $category_question->category_question_key    =   $result['question_key'];
+           $category_question->category_question_value  =   $result['question_value'];
+           $category_question->save();
+           $q[] = ['question_key'=>$result['question_key'],'question_value'=>$result['question_value']];
+        }
+        unset($postTask->id);
+        $postTask->category_question=$q;
 
         return response()->json(
                                     [ 
